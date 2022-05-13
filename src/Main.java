3bbc165508a0046828bfef4e8e01becb16f95630
stevensons1061 <shanestevenson04@gmail.com>
2022-05-13 12:21:27 -0700

    s

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+
+        JFrame f = new JFrame();
+        f.setVisible(true);
+        f.setSize(800, 450);
+        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        System.out.println("HHH");
 
     }
 }
