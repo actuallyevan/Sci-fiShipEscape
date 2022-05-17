904bd09adb729781b11830c3c277ead924523eee
fakel <shanestevenson04@Gmail.com>
2022-05-16 18:57:13 -0700

    uhh wait a second

--- src/Board.java
+++ src/Board.java
@@ -1,11 +1,14 @@
+import java.awt.*;
+
 public class Board {
 
     private int[][] grid;
-    private int[] tiles;
+    private Color[] tiles;
 
     public Board(int [][] grid) {
         this.grid = grid;
-
+        this.tiles = new Color[] {Color.darkGray, Color.black, Color.red};
+        System.out.println(tiles[1]);
     }
 
 }
