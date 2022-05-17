bbcd0f50721fc69673ff96598a0ec88bdeecb475
fakel <shanestevenson04@Gmail.com>
2022-05-16 22:02:30 -0700

    First iteration of tiles (only colors)

--- src/Wall.java
+++ src/Wall.java
@@ -4,13 +4,13 @@ public class Wall extends Tile {
 
     Color tileColor = Color.black;
 
-    public Wall () {
+    public Wall (int xpos, int ypos) {
         super(xpos, ypos);
     }
     @Override
     public void draw (Graphics g) {
         g.setColor(tileColor);
-        g.fillRect(xpos, ypos, , wallHeight);
+        g.fillRect(xpos, ypos, tileHeight, tileWidth);
     }
 
 
